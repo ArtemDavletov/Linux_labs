be9bee6aa261518e2d570121c14a50a60c10b12d
Artem Davletov <Tomas-Cooper@mail.ru>
2020-11-04 20:01:56 +0300

    Add third lab

---
+++ lab3/del.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+userdel u1
+userdel u2
+groupdel g1
+rm -rf test15
+rm -rf /root/test14
+rm -rf /root/test13
+rm -rf /root/u2
+rm -rf /root/u1
+rm -f /etc/skel/readme.txt
+rm -f work3.log
